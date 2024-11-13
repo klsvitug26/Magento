7ef98da1593beacceab5ec15ae42e15ca9c4503c
Kim.Vitug <Kim.Vitug@PHL3LF2SV3.asia.essilor.group>
2024-11-13 09:46:18 +0800

    Change city field value

--- src/main/java/data/newaddress.java
+++ src/main/java/data/newaddress.java
@@ -4,7 +4,7 @@ public class newaddress {
 	
 	public static String company = "New Company 1";
 	public static String address = "New Address 1";
-	public static String city = "New City 1";
+	public static String city = "New City 2";
 	public static String zipCode = "31201";
 	public static String phone = "091234567899";
 	public static String provincePH = "Laguna";
